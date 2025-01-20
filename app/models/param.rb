519e6e2c4e0770d8e082626358022d95480787e9
JP <work.jortiz@gmail.com>
2025-01-20 10:08:06 -0600

    add next number logic

--- app/models/param.rb
+++ app/models/param.rb
@@ -1,2 +1,14 @@
 class Param < ApplicationRecord
+
+    def self.generate_nn(model_name)
+        this_param = Param.find_by_name(model_name)
+        cc = 0
+        if this_param.present? 
+            cc = this_param.int_content
+            this_param.int_content += 1
+            this_param.save
+        end
+
+        cc
+    end
 end
