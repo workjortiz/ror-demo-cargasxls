24e2c66415028695f325a24baeda1f4ee97776c6
JP <work.jortiz@gmail.com>
2025-02-27 12:38:59 -0600

    Improve the flow after load massive data show results in other page and allow revise older loads.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   def import
     if params[:file].present?
       instance_load = ProductsExcelImporter.new(params[:file]).import
-      redirect_to products_path(:instance_load => instance_load), notice: "Proceso de carga Excel carga completado"
+      redirect_to products_load_data_result_path(:instance_load => instance_load), notice: "Proceso de carga Excel carga completado"
     else
       redirect_to products_path, alert: "No hay archivo adjunto"
     end
@@ -12,13 +12,20 @@ class ProductsController < ApplicationController
   
   # GET /products or /products.json
   def index
-    @array_bit_error_log = params[:instance_load].present? ? BitLoadData.where(event: "LOAD_EXCEL", model: "PRODUCTS", action: "NEW/ERROR", data1: params[:instance_load]).order(value1: :desc) : nil
-    @array_bit_ok_log = params[:instance_load].present? ? BitLoadData.where(event: "LOAD_EXCEL", model: "PRODUCTS", action: "NEW/COMPLETE", data1: params[:instance_load]).order(value1: :desc) : nil
-    @products = Product.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+   @products = Product.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
     respond_to do |format|
       format.html
       format.xlsx
     end
+  end
+
+  def load_data_result
+    @array_bit_error_log = params[:instance_load].present? ? BitLoadData.where(event: "LOAD_EXCEL", model: "PRODUCTS", action: "NEW/ERROR", data1: params[:instance_load]).order(value1: :desc) : nil
+    @array_bit_ok_log = params[:instance_load].present? ? BitLoadData.where(event: "LOAD_EXCEL", model: "PRODUCTS", action: "NEW/COMPLETE", data1: params[:instance_load]).order(value1: :desc) : nil
+  end
+
+  def load_data_index
+    @bit_history = BitLoadData.select("DISTINCT data1").where(event: 'LOAD_EXCEL', model: 'PRODUCTS').order("data1 DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def info
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  # Defines the root path route ("/")
+  root "shared#home"
+
+  # Non CRUD Pages
+  get "shared/home" => "shared#home", :as => "shared_home"
+
+  # Models
+  resources :unit_measures
+  resources :brands
+  resources :countries
   resources :params
   resources :products do
     collection { 
@@ -6,13 +17,13 @@ Rails.application.routes.draw do
     }
     
   end
+
+  # Custom
   get "product/info" => "products#info", :as => "product_info"
+  get "products/load-data/result" => "products#load_data_result", :as => "products_load_data_result"
+  get "products/load-data/index" => "products#load_data_index", :as => "products_load_data_index"
 
-  resources :unit_measures
-  resources :brands
-  resources :countries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -21,8 +32,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  get "shared/home" => "shared#home", :as => "shared_home"
+  
 
-  # Defines the root path route ("/")
-  root "shared#home"
+  
 end
